b4fff51e5deee0602f8428bea2e738c2ad45cbc6
muhammad hamri <muhammadhamri1977@gmail.com>
2023-09-24 08:14:11 +0700

    coba

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// const backsound = document.getElementById("backsound");
+// const sound = document.getElementById("sound");
+
+// // Menambahkan event listener untuk memainkan audio setelah klik pertama
+// sound.addEventListener("click", function () {
+//   backsound.play();
+//   // Hapus event listener agar tidak memainkan ulang setiap kali ada klik
+//   sound.removeEventListener("click", arguments.callee);
+// });
+
 const backsound = document.getElementById("backsound");
 const sound = document.getElementById("sound");
 
@@ -6,4 +16,10 @@ sound.addEventListener("click", function () {
   backsound.play();
   // Hapus event listener agar tidak memainkan ulang setiap kali ada klik
   sound.removeEventListener("click", arguments.callee);
+
+  // Menambahkan event listener untuk menghentikan audio saat pengguna keluar dari halaman
+  window.addEventListener("beforeunload", function () {
+    backsound.pause();
+    backsound.currentTime = 0; // Mengatur posisi audio ke awal
+  });
 });
